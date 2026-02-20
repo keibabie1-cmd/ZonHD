85abe4af8f26f05abce07d44bed7cb3a9919baad
keibabie1-cmd <juice.ahdijuice@gmail.com>
2026-02-19 20:55:41 -0500

    server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.get('/api/config', (req, res) => {
-    res.json({ tmdb_key: TMDB_KEY });
+    res.json({ tmdb_key: TMDB_KEY });
 });
 
 const PORT = process.env.PORT || 3000;
